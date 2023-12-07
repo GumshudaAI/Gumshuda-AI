c4bb96fbab47110f692ce7e4de08652ca860ff34
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-12-08 00:16:57 +0530

    Adding a function to upload PIL image to imgBB

--- backend/main.py
+++ backend/main.py
@@ -37,3 +37,32 @@ index_name="final-database"
 if index_name not in pinecone.list_indexes():
     pinecone.create_index(index_name, dimension=512, metric="dotproduct", pod_type="s1")
 index = pinecone.Index(index_name)
+
+
+# Temporary text for BM25 fitting
+temp = "Import errors are caused due to circular imports most of the time"
+_id = 100001
+bm25.fit(temp)
+
+# Connect to Img DB
+imgbb_api_key = os.getenv("IMG_DB_API_KEY")
+
+# Convert PIL image to base64 and upload to Img BB
+async def get_url(image_content):
+    encoded_image = base64.b64encode(image_content).decode("utf-8")
+    imgbb_api_url = "https://api.imgbb.com/1/upload"
+    data = {
+        "key": imgbb_api_key,
+        "image": encoded_image
+    }
+    response = requests.post(imgbb_api_url, data=data)
+
+    # Get the URL of the uploaded image
+    if response.status_code == 200:
+        result = response.json()
+        image_url = result["data"]["url"]
+        return image_url
+    else:
+        print(f"Failed to upload image. Status code: {response.status_code}")
+        raise HTTPException(status_code=response.status_code, detail='Failed to upload image')
+        return ''
